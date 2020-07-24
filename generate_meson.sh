e4cd2313517f61bd8dacae2123176fd525929e29
Antony Chan <antony@truepic.com>
2020-07-23 17:55:23 -0700

    Add comma

--- generate_meson.sh
+++ generate_meson.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "$(basename $1)_dep = declare_dependency("
-echo "include_directories: '.'"
+echo "include_directories: '.',"
 echo "sources: ["
 for file in *.c; do
     echo "'${file}',"
